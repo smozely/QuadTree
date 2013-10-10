18283ab3946dcb899a37aa0c5f33962798e09f50
Steve Mosley <steven.mosley1@gmail.com>
2013-10-10 22:29:49 +1300

    Update rectangle to make it obvious the expected dimensions.

--- src/main/java/quadTree/Rectangle.java
+++ src/main/java/quadTree/Rectangle.java
@@ -2,11 +2,11 @@ package quadTree;
 
 public class Rectangle {
 	public float x1, x2, y1, y2;
-	
+
 	public Rectangle() {
-		this(Float.POSITIVE_INFINITY, Float.POSITIVE_INFINITY, Float.NEGATIVE_INFINITY, Float.NEGATIVE_INFINITY);
+		this(Float.NEGATIVE_INFINITY, Float.NEGATIVE_INFINITY, Float.POSITIVE_INFINITY, Float.POSITIVE_INFINITY);
 	};
-	
+
 	public Rectangle(float _x1, float _y1, float _x2, float _y2) {
 		this.x1 = _x1;
 		this.y1 = _y1;
